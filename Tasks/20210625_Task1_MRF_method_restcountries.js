78434d5435abc0cd27d6f6ff09529b0a92c1b728
sridharrajaram <85097513+sridharrajaram@users.noreply.github.com>
2021-06-28 13:23:56 +0530

    Add files via upload

---
+++ Tasks/20210625_Task1_MRF_method_restcountries.js
@@ -0,0 +1,48 @@
+/* 1) Solving problems using array functions on rest countries data. 
+        - Get all the countries from Asia continent /region using Filter function
+        - Get all the countries with a population of less than 2 lakhs using Filter function
+        - Print the following details name, capital, flag using forEach function 
+        - Print the total population of countries using reduce function 
+        - Print the country which use US Dollars as currency. */
+
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+        xhr.onload = function () {
+          var data = JSON.parse(this.response);
+          console.log(data);
+
+          //Get all the countries from Asia continent /region using Filter function
+          var asianRegion = data.filter(ele=>{
+            if(ele.region === "Asia")
+            {
+              return ele;
+            }});
+          console.log(asianRegion);
+
+          //Get all the countries with a population of less than 2 lakhs using Filter function
+          let popcheck = 200000;
+          var counPop = data.filter(ele => {
+            if(ele.population<popcheck)
+            {
+              return ele;
+            }
+          });
+          console.log(counPop);
+          
+          //Print the following details name, capital, flag using forEach function
+          data.forEach((ele)=>{
+            console.log(ele.name,ele.capital,ele.flag);
+          });
+          
+          //Print the total population of countries using reduce function
+          var totalPop = data.map(ele => {return ele.population}).reduce(((a,b)=> a+b),0);
+          console.log(totalPop);
+
+/*           //Print the country which use US Dollars as currency.
+          let dollarCoun = data.map(ele => ele.currencies);
+          console.log(dollarCoun);
+*/
+
+        };
+        xhr.send();
